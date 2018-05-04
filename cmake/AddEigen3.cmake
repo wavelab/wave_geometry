c482c6fbcfdd94fc2e0e8f41f8739f7a5f052a7d
Leo Koppel <lkoppel@uwaterloo.ca>
2018-05-04 16:24:51 -0400

    Add FindEigen3 module for older Eigen

--- cmake/AddEigen3.cmake
+++ cmake/AddEigen3.cmake
@@ -1,8 +1,16 @@
 # Make the Eigen3::Eigen target available, either by finding a new version of Eigen or by
 # wrapping an old version of Eigen.
 
-find_package (Eigen3 REQUIRED)
+# Try config-file search only
+find_package(Eigen3 3.2.92 QUIET NO_MODULE)
+if(Eigen3_FOUND)
+  message(STATUS "Found Eigen: ${EIGEN3_INCLUDE_DIR}")
+else()
+  # Try using FindEigen3.cmake (needed for older distributions)
+  find_package(Eigen3 3.2.92 REQUIRED)
+endif()
 
+# For older Eigen there may not be a Eigen3::Eigen target. Make one.
 if (NOT TARGET Eigen3::Eigen)
   add_library(Eigen3::Eigen INTERFACE IMPORTED)
   set_property(TARGET Eigen3::Eigen PROPERTY
@@ -10,5 +18,3 @@ if (NOT TARGET Eigen3::Eigen)
   set_property(TARGET Eigen3::Eigen PROPERTY
     INTERFACE_COMPILE_DEFINITIONS ${EIGEN3_DEFINITIONS})
 endif (NOT TARGET Eigen3::Eigen)
-
-message(STATUS "Found Eigen: ${EIGEN3_INCLUDE_DIR}")
