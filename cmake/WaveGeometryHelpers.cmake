27be85b37f25b8bb7ec4cd3fc189d3fc9fb7361d
Leo Koppel <lkoppel@uwaterloo.ca>
2018-08-02 16:08:27 -0400

    CMake: allow building inside libwave (#4)
    Changes to allow building as a module of libwave (https://github.com/wavelab/libwave). No behaviour change is expected for users of `wave_geometry` by itself.
    
    Specific changes: 
    - Check for `TARGET wave` to differentiate between standalone build and building as a subdirectory of libwave. In that case, we don't define all our own install targets and CMake config files.
    - Rename helper functions to avoid name clash with libwave CMake functions.
    - Split CMake config file generation into `InstallConfigFiles.cmake`
    
    For now, some functionality is duplicated (e.g. ADD_TEST helper functions) and could be simplified in future.

--- cmake/WaveGeometryHelpers.cmake
+++ cmake/WaveGeometryHelpers.cmake
@@ -3,24 +3,24 @@
 INCLUDE(CMakeParseArguments)
 INCLUDE(GNUInstallDirs)
 
-# wave_add_test: Add a gtest target
+# wave_geometry_add_test: Add a gtest target
 #
-# WAVE_ADD_TEST(Name [DISABLED] src1 [src2...])
+# WAVE_GEOMETRY_ADD_TEST(Name [DISABLED] src1 [src2...])
 #
 # The test will be added to the tests run by "make test", unless DISABLED is
 # given. It will be linked against the needed gtest libraries. Any other links
 # can be made separately with the target_link_libraries command.
-FUNCTION(WAVE_ADD_TEST NAME)
+FUNCTION(WAVE_GEOMETRY_ADD_TEST NAME)
 
     # Define the arguments this function accepts
     SET(options DISABLED)
     SET(one_value_args "")
     SET(multi_value_args "")
-    CMAKE_PARSE_ARGUMENTS(WAVE_ADD_TEST
+    CMAKE_PARSE_ARGUMENTS(WAVE_GEOMETRY_ADD_TEST
         "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
     # Build the test executable using the given sources
-    ADD_EXECUTABLE(${NAME} ${WAVE_ADD_TEST_UNPARSED_ARGUMENTS})
+    ADD_EXECUTABLE(${NAME} ${WAVE_GEOMETRY_ADD_TEST_UNPARSED_ARGUMENTS})
 
     # Link gtest libraries including one providing main()
     TARGET_LINK_LIBRARIES(${NAME} wave_geometry GTest::Main)
@@ -32,21 +32,21 @@ FUNCTION(WAVE_ADD_TEST NAME)
     # Build this test on "make tests"
     ADD_DEPENDENCIES(tests ${NAME})
 
-    IF(NOT WAVE_ADD_TEST_DISABLED)
+    IF(NOT WAVE_GEOMETRY_ADD_TEST_DISABLED)
         # Add the executable as a test, so it runs with "make test"
         ADD_TEST(NAME ${NAME} COMMAND ${NAME})
     ENDIF()
 
-ENDFUNCTION(WAVE_ADD_TEST)
+ENDFUNCTION(WAVE_GEOMETRY_ADD_TEST)
 
-# wave_add_benchmark: Add a target which links against google benchmark
+#     wave_geometry_add_benchmark: Add a target which links against google benchmark
 #
-# WAVE_ADD_BENCHMARK(Name src1 [src2...])
+# WAVE_GEOMETRY_ADD_BENCHMARK(Name src1 [src2...])
 #
 # The target will be added to the tests run by "make benchmark". It will be
 # linked against the needed libraries. Any other links can be made separately
 # with the target_link_libraries command.
-FUNCTION(WAVE_ADD_BENCHMARK NAME)
+FUNCTION(WAVE_GEOMETRY_ADD_BENCHMARK NAME)
     # Build the executable using the given sources
     ADD_EXECUTABLE(${NAME} ${ARGN})
 
@@ -65,4 +65,4 @@ FUNCTION(WAVE_ADD_BENCHMARK NAME)
 
     # Build this target on "make benchmarks"
     ADD_DEPENDENCIES(benchmarks ${NAME})
-ENDFUNCTION(WAVE_ADD_BENCHMARK)
+ENDFUNCTION(WAVE_GEOMETRY_ADD_BENCHMARK)
