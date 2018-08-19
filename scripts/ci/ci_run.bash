dddc606fb0b0627aaf6b30192013c97488ebed49
Leo Koppel <lkoppel@uwaterloo.ca>
2018-08-19 02:34:13 -0400

    Change travis build environment
    This is a combination of 3 commits.
    
    - Switch travis environments
    
      The "sanitize" build often runs out of memory on Travis. Try using
    sudo-enabled environment because it has more memory.
    
      Also disable ccache. There is no point for a header-only library (except for
    rare commits which don't touch the headers).
    
    - Fix travis build type
    
    - Disable misleading-indentation warning in travis
    
      I can't reproduce locally; happens on travis somewhere inside a macro.

--- scripts/ci/ci_run.bash
+++ scripts/ci/ci_run.bash
@@ -8,9 +8,9 @@ compile_libwave() {
     cd build
     cmake .. -DCMAKE_MODULE_PATH=$DIR/$CMAKE_CONFIG_DIR \
         -DCMAKE_CXX_FLAGS="-Werror $WAVE_CXX_FLAGS"     \
-        -DCMAKE_BUILD_TYPE=Release                      \
+        -DCMAKE_BUILD_TYPE=${WAVE_BUILD_TYPE:=Release}  \
         -DCMAKE_VERBOSE_MAKEFILE=ON
-    make -j2  # Travis gives two cores
+    make -j${WAVE_JOBS:=2}  # Travis gives two cores (can override with $WAVE_JOBS)
 
     # (don't rely on nproc: https://github.com/travis-ci/travis-ci/issues/6859)
 }
