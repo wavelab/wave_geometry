d0fd6967a4a2f8f70d991be42762488f748fc40a
Leo Koppel <lkoppel@uwaterloo.ca>
2018-10-20 00:28:58 -0400

    Add branch whitelist for docs

--- scripts/ci/ci_make_docs.bash
+++ scripts/ci/ci_make_docs.bash
@@ -5,8 +5,11 @@ set -e  # exit on first error
 : ${DOCS_BUILD_DIR:=build}
 
 # Build docs for all tagged versions
-# Always build docs on the current branch, though we only deploy from master
-sphinx-versioning build -r $TRAVIS_BRANCH docs $DOCS_BUILD_DIR -- -W
+# Always build docs on the current branch, though we only deploy from some builds
+# Build docs for: master, develop, current branch, version tags
+sphinx-versioning build -r $TRAVIS_BRANCH --whitelist-branches ^${TRAVIS_BRANCH}$ \
+  --whitelist-branches ^develop$ --whitelist-branches ^master$ --whitelist-tags ^[0-9] \
+  docs $DOCS_BUILD_DIR -- -W
 
 # Add .nojekyll to stop GitHub Pages from excluding underscore-prefixed files
 touch $DOCS_BUILD_DIR/.nojekyll
